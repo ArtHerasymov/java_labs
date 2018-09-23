0f7adebaeb5c429d7a3fd5487ce842214ecc2096
dr10n9 <dr10n9@gmail.com>
2018-09-23 19:45:55 +0000

    lab1 task1

--- lab1/task1/src/Main.java
+++ lab1/task1/src/Main.java
@@ -1,1 +1,7 @@
-public class Main{}
+package lab1.task1;
+import lab1.task1.Applet;
+public class Main{
+    public static void main(String[] args){
+        new Applet().getWindow("TestSurname");
+    }
+}
