78a5bd895f47814452003a065d73e3947813b2bb
dr10n9 <dr10n9@gmail.com>
2018-09-23 19:06:25 +0000

    task2 added Main to package

--- lab1/task2/src/Main.java
+++ lab1/task2/src/Main.java
@@ -1,1 +1,8 @@
-public class Main{}
+package lab1.task2;
+import lab1.task2.Number;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println(new Number().isLucky(123));
+    }
+}
